badbd38bfdb16e39b201e498a5c9a72c6216c27f
Timon <timonvanwaardhuizen@gmail.com>
2019-09-26 21:09:10 +0200

    Migrations and models added
    added: Tasks, Times and Groups

---
+++ TimeManagementSystem/database/migrations/2019_09_26_185322_create_times_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->integer('task_id')->unsigned();
+            $table->integer('group_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+}
